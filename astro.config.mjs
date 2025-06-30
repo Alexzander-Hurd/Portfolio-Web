9889f2349ae8b430747433dad06e690721f677c6
Alexzander Hurd <ajghurd@gmail.com>
2025-06-30 15:28:11 +0100

    adding remark formatting packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,24 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import remarkWikiLink from 'remark-wiki-link';
+import remarkObsidian from 'remark-obsidian';
+import slugify from 'slugify';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://alexhurd.com',
 	integrations: [mdx(), sitemap()],
+	markdown: {
+		remarkPlugins: [
+			[remarkWikiLink, { 
+				hrefTemplate: permalink => `/blog/${permalink}`,
+				pageResolver: name => slugify(name, { lower: true, strict: true }),
+			}],
+			[remarkObsidian, {
+				aliasDelimiter: '|',
+			}]
+		],
+
+	},
 });
