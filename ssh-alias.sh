584ad312fc38d7eb999d41c82df88007a6d92f0e
Alex Hurd <ajghurd@gmail.com>
2025-07-06 03:45:12 +0100

    added logging to ssh-alias

--- ssh-alias.sh
+++ ssh-alias.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
+
+echo "Starting ssh-alias.sh"
 if [ -z "$SSH_KEY" ]; then
     echo "SSH_KEY environment variable not set"
     exit 0
 fi
 
+echo "SSH_KEY environment variable set"
+
 mkdir -p ~/.ssh
 echo "$SSH_KEY" > ~/.ssh/id_ed25519
 chmod 600 ~/.ssh/id_ed25519
+
+echo $(ll ~/.ssh/)
 
 # SSH alias for content repo
 cat <<EOF > ~/.ssh/config
@@ -17,3 +23,5 @@ Host content.github.com
     IdentityFile ~/.ssh/id_ed25519
     StrictHostKeyChecking no
 EOF
+
+echo "SSH alias created"
